43df1068cd9d3673e86ccb13a2a81d84d132d710
Hilário Cunha <hilario.cunha@gmail.com>
2017-08-01 22:48:55 +0100

    Problem 3 solutions

---
+++ Problem 3.hs
@@ -0,0 +1,23 @@
+{-|
+(*) Find the K'th element of a list. The first element in the list is number 1.
+
+Example:
+
+* (element-at '(a b c d e) 3)
+c
+Example in Haskell:
+
+Prelude> elementAt [1,2,3] 2
+2
+Prelude> elementAt "haskell" 5
+'e'
+-}
+elementAt :: [a] -> Int -> a
+elementAt xs i = xs !! (i - 1)
+
+elementAt' :: [a] -> Int -> a
+elementAt' xs i 
+  | length xs < i = error "Index out of bounds"
+  | i < 1 = error "Index out of bounds"
+  | otherwise = xs !! (i - 1)
+
